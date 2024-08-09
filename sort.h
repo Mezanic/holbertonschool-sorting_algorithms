5a20d67eba8aec8d3136701e85eb8153e73c221c
Mezanic <ezanic.mael@gmail.com>
2024-08-09 17:27:06 +0200

    Betty style

--- sort.h
+++ sort.h
@@ -29,4 +29,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void recursive(int *array, size_t size, ssize_t start, ssize_t end);
-#endif 
+#endif
